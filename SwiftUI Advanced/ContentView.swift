bf8f3a7250b3685f4a852192a7d3cf9b32e767ab
Abhishek Saral <abhisheksaral@gmail.com>
2021-07-17 19:26:51 -0700

    Initial Commit

--- SwiftUI Advanced/ContentView.swift
+++ SwiftUI Advanced/ContentView.swift
@@ -6,75 +6,41 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
 
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        ZStack {
+            Image("background-3")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea(.all)
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Sign Up")
+                        .font(Font.largeTitle.bold())
+                        .foregroundColor(.white)
+                    Text("Access to 120+ hours of courses, tutorials and livestreams")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white.opacity(0.7))
+                }
+                .padding(20)
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .background(
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(Color.white.opacity(0.2))
+                .background(Color("secondaryBackground").opacity(0.5))
+                .background(VisualEffectBlur(blurStyle: .systemThinMaterialDark))
+                .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+                .cornerRadius(30)
+                .padding(.horizontal)
+        )
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
